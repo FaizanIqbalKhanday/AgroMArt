8f147b366f87a3c3af6019d58d5465a4b392f751
Imran00852 <bhatimran9797@gmail.com>
2023-07-28 22:07:49 +0530

    added SetNewPassword and Successful msg screens

---
+++ app/src/main/java/in/codecubes/agromart/ForgetPasswordSuccessMessage.java
@@ -0,0 +1,14 @@
+package in.codecubes.agromart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgetPasswordSuccessMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password_success_message);
+    }
+}
--- app/src/main/java/in/codecubes/agromart/SplashActivity.java
+++ app/src/main/java/in/codecubes/agromart/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, VerifyOTP.class));
+                startActivity(new Intent(SplashActivity.this, SetNewPassword.class));
             }
         }, 3000);
     }
