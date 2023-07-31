89987356b2d7d61c5bbb59e4bd0232070abd078c
Faizan Iqbal <faizan>
2023-07-31 16:34:10 +0530

    main activity UI design

--- app/src/main/java/in/codecubes/agromart/SplashActivity.java
+++ app/src/main/java/in/codecubes/agromart/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, SetNewPassword.class));
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
             }
         }, 3000);
     }
